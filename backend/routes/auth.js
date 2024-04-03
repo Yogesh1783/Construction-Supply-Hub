baae850cb6143a299ea324b4700b0b525f47f0d1
Yogesh1783 <Ydp143452@gmail.com>
2024-04-03 16:13:45 +0530

    changed rouute

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,6 +7,7 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser);
 router.route("/password/forgot").post(forgotPassword);
+router.route("/password/forgottets").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 
