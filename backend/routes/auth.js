7308aef9db7584bf0efb7b36c4729453db0f43a1
Yogesh1783 <Ydp143452@gmail.com>
2024-04-03 16:25:29 +0530

    romved invalid route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,7 +7,6 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser);
 router.route("/password/forgot").post(forgotPassword);
-router.route("/password/forgottets").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 
