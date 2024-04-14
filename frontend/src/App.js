c45af73995f2c1ec2b55ea2ed21df2c640a4f971
Yogesh1783 <Ydp143452@gmail.com>
2024-04-14 18:15:02 +0530

    cart option is implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,6 +21,9 @@ import Cart from "./components/cart/Cart";
 import Shipping from "./components/cart/Shipping";
 import ConfirmOrder from "./components/cart/ConfirmOrder";
 import PaymentMethod from "./components/cart/PaymentMethod";
+import MyOrders from "./components/order/MyOrders";
+import OrderDetails from "./components/order/OrderDetails";
+import Invoice from "./components/invoice/Invoice";
 
 function App() {
   return (
@@ -80,6 +83,12 @@ function App() {
           <Route path="/shipping" element={<ProtectedRoute><Shipping /></ProtectedRoute>} />
           <Route path="/confirm_order" element={<ProtectedRoute><ConfirmOrder /></ProtectedRoute>} />
           <Route path="/payment_method" element={<ProtectedRoute><PaymentMethod /></ProtectedRoute>} />
+          <Route path="/me/orders" element={<ProtectedRoute><MyOrders /></ProtectedRoute>} />
+          <Route path="/me/order/:id" element={<ProtectedRoute><OrderDetails /></ProtectedRoute>} />
+          <Route path="/invoice/order/:id" element={<ProtectedRoute><Invoice /></ProtectedRoute>} />
+
+
+
 
 
           </Routes>
